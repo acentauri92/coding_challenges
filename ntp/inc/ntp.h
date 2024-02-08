e9377fe75bb62fbc340e9a3fa77863209890ad14
Dheeraj Reddy J <dheerajreddy_j@yahoo.in>
2024-02-08 13:33:06 +0530

    Updated the NTP header and added time conversion funcs.
    NTP time format structure updated internally in the
    ntp header. ynix-ntp time conversion functions added
    and tested.
    
    TODO:
     1. Socket functions
     2. Update ntp header to use network byte order.

--- ntp/inc/ntp.h
+++ ntp/inc/ntp.h
@@ -1,28 +1,57 @@
 #ifndef NTP_H
 #define NTP_H
+
 #include <stdint.h>
 
+/*Offset between NTP and Unix time*/
+#define NTP_EPOCH_OFFSET    ( (70UL * 365UL + 17UL) * 86400UL )
+
+#define NTP_LEAP_INDICATOR_BIT_POSITION     6
+#define NTP_VERSION_BIT_POSITION            3
+#define NTP_MODE_BIT_POSITION               0
+/* NTP Leap indicator warnings*/
 typedef enum{
-    NO_WARNING = 0,
-    LAST_MINUTE_HAS_61_S,    
-    LAST_MINUTE_HAS_59_S,    
-    UNSYNCHRONIZED_MODE     
+    NTP_NO_WARNING = 0,
+    NTP_LAST_MINUTE_HAS_61_S,    
+    NTP_LAST_MINUTE_HAS_59_S,    
+    NTP_UNSYNCHRONIZED_MODE     
 }leap_indicator_t;
 
-typedef struct {
+typedef enum{
+    NTP_VERSION_3 = 3,
+    NTP_VERSION_4
+}ntp_version_t;
+
+/*NTP Modes*/
+typedef enum{
+    NTP_RESERVED = 0,
+    NTP_SYMMETRIC_ACTIVE,
+    NTP_SYMMETRIC_PASSIVE,
+    NTP_CLIENT,
+    NTP_SERVER,
+    NTP_BROADCAST,
+    NTP_NTP_CONTROL_MESSAGE,
+    NTP_RESERVED_PRIVATE
+}ntp_mode_t;
+
+typedef struct __attribute__((packed)){
+    uint32_t seconds;
+    uint32_t fraction;
+}ntpTimeFormat_t;
+
+typedef struct __attribute__((packed)){
     uint8_t li_vn_mode;         // Leap Indicator, Version Number, Mode
     uint8_t stratum;            // Stratum
     uint8_t poll;              
     int8_t precision;           
     uint32_t rootDelay;        
     uint32_t rootDispersion;   
-    uint32_t refID;           
-    uint64_t refTimestamp;     
-    uint64_t origTimestamp;    
-    uint64_t rxTimestamp;    
-    uint64_t txTimestamp;
+    uint32_t refID;   
+
+    ntpTimeFormat_t refTimestamp;     
+    ntpTimeFormat_t origTimestamp;    
+    ntpTimeFormat_t rxTimestamp;    
+    ntpTimeFormat_t txTimestamp;
 }ntpHeader_t;
 
-
-
-#endif
+#endif // NTP_H
--- ntp/src/ntp.c
+++ ntp/src/ntp.c
@@ -6,6 +6,9 @@ ntpHeader_t ntpHeader;
 
 struct timeval time;
 
+static void timevalToNtp(struct timeval* tv, ntpTimeFormat_t* ntptimeformat);
+static void ntp_to_timeval(struct timeval* tv, ntpTimeFormat_t* ntptimeformat);
+
 int main(void){
 
 
@@ -14,11 +17,28 @@ int main(void){
     printf("tvsec: %ld\n", time.tv_sec);
     printf("tv usec: %ld\n", time.tv_usec);
 
+    /* Set the first 8 bits of the header*/
+    ntpHeader.li_vn_mode |= NTP_UNSYNCHRONIZED_MODE << NTP_LEAP_INDICATOR_BIT_POSITION;
+    ntpHeader.li_vn_mode |= NTP_VERSION_4 << NTP_VERSION_BIT_POSITION;
+    ntpHeader.li_vn_mode |= NTP_CLIENT << NTP_MODE_BIT_POSITION;
 
-    // ntpHeader.li_vn_mode |= UNSYNCHRONIZED_MODE << 6;
-    // ntpHeader.li_vn_mode |= 4 << 3;
+    timevalToNtp(&time, &(ntpHeader.origTimestamp));
 
-
-    // printf("Value of li: %u\n", ntpHeader.li_vn_mode);
+    printf("ntp time format(s): %u\n", ntpHeader.origTimestamp.seconds);
+    printf("ntp time format(ns): %u\n", ntpHeader.origTimestamp.fraction);
 
 }
+
+static void timevalToNtp(struct timeval* tv, ntpTimeFormat_t* ntptimeformat){
+    ntptimeformat->seconds = tv->tv_sec + NTP_EPOCH_OFFSET;
+       
+    ntptimeformat->fraction = (uint32_t)((double)(tv->tv_usec + 1) * \
+                            (double)(1LL << 32) * 1.0e-6);
+}
+
+static void ntp_to_timeval(struct timeval* tv, ntpTimeFormat_t* ntptimeformat){
+    tv->tv_sec = ntptimeformat->seconds - 2208988800;
+    tv->tv_usec = (uint32_t)((double)ntptimeformat->fraction * 1.0e6 / \
+                    (double)(1LL << 32));
+}
+ 
